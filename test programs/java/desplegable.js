c6953be7bf92ee6dfc1c275740c3bf961920739d
Seresfantasticos <84489477+Seresfantasticos@users.noreply.github.com>
2021-05-20 16:27:15 -0500

    Add files via upload

---
+++ test programs/java/desplegable.js
@@ -0,0 +1,4 @@
+let btnmenu = document.getElementById('btnmenu');
+let items = document.getElementById('items');
+btnmenu.addEventListener('click', function(){ 'use strict'; items.classList.toggle('mostrar');
+});
